67b3fc558f17d300b9c9479aa91c95e68241bf3c
AnnemarieFulcer <aef22@njit.edu>
2018-07-03 17:41:15 -0400

    added route so that authentification /login and register work

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -15,6 +15,12 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route('contact')}}">Contact</a>
             </li>
-
+            <li class="nav-item">
+                 <a class="nav-link" href="{{ route('login') }}">Login</a>
+             </li>
+            <li class="nav-item">
+            <a  clas="nav-link" href="{{ route('register') }}">Register</a>
+            </li>
+        </ul>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', 'PagesController@home')->name('home');
 Route::get('/about','PagesController@about' )->name('about');
 
 Route::get('/contact', 'PagesController@contact')->name('contact');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
