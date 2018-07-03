8586b1c8243157c208f1521fb93d8be67be8686a
AnnemarieFulcer <aef22@njit.edu>
2018-07-03 17:13:25 -0400

    5. Unit test to insert a user

---
+++ tests/Unit/UsersTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function insertUser()
+    {
+        $user=new User();
+        $this->assertTrue($user->save());
+    }
+}
+
