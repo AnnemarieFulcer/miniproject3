bc33710bcca95a6366d1f7dcc17b77009e5ff0a5
AnnemarieFulcer <aef22@njit.edu>
2018-07-06 17:25:10 -0400

    Fix register link appearance in nav menu

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -19,7 +19,7 @@
                  <a class="nav-link" href="{{ route('login') }}">Login</a>
              </li>
             <li class="nav-item">
-            <a  clas="nav-link" href="{{ route('register') }}">Register</a>
+            <a  class="nav-link" href="{{ route('register') }}">Register</a>
             </li>
         </ul>
     </div>
