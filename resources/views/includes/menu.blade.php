7cb94f715b45dece46362a3e31d869152bf93827
AnnemarieFulcer <aef22@njit.edu>
2018-07-06 17:53:41 -0400

    fixed a typo

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Mini Project 2</a>
+    <a class="navbar-brand" href="#">Mini Project 3</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
